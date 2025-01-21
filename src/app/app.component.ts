4c5c622ed5444aff67818d587d928c754b78f07c
Miguel Escoz <miguel.escoz@nter.es>
2025-01-21 13:52:05 +0100

    Añadida página de control de flujos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
 	selector: 'app-root',
@@ -8,6 +9,13 @@ import {RouterOutlet} from '@angular/router';
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+	private primeConfig = inject(PrimeNGConfig);
+
 	title = 'my-dashboard-app-ng';
+
+	ngOnInit(): void {
+		this.primeConfig.ripple = true;
+	}
 }
--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CardModule} from 'primeng/card';
+import {DividerModule} from 'primeng/divider';
+import {Button} from 'primeng/button';
+import {Ripple} from 'primeng/ripple';
+
+type Grade = 'A' | 'B' | 'C' | 'F';
 
 @Component({
 	standalone: true,
-	imports: [],
+	imports: [
+		CardModule,
+		DividerModule,
+		Button
+	],
 	templateUrl: './control-flow.component.html',
 	styles: ``
 })
 export default class ControlFlowComponent {
 
+	public showContent = signal(false);
+	public grade = signal<Grade>('B');
+
+	public toggleContent(): void {
+		this.showContent.update(value => !value);
+	}
 }
