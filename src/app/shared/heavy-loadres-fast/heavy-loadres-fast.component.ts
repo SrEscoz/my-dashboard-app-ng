6a99cd5855cb25c72175f9944fcd779bd944fd2c
Miguel Escoz <miguel.escoz@nter.es>
2025-01-21 09:34:25 +0100

    Añadida estructura de rutas y archivos

---
+++ src/app/shared/heavy-loadres-fast/heavy-loadres-fast.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-heavy-loadres-fast',
+	standalone: true,
+	imports: [],
+	templateUrl: './heavy-loadres-fast.component.html',
+	styles: ``
+})
+export class HeavyLoadresFastComponent {
+
+}
