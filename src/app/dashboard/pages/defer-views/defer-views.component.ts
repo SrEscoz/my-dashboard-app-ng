a25da1f9c945c60af817f96ca5d14f341483718a
Escoz <kyrovilches@gmail.com>
2025-01-24 12:15:24 +0100

    Carga de vistas deferidas en defer-views.component

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,19 @@
 import {Component} from '@angular/core';
+import {DashboardHeaderComponent} from '../../../shared/dashboard-header/dashboard-header.component';
+import {HeavyLoadersSlowComponent} from '../../../shared/heavy-loaders-slow/heavy-loaders-slow.component';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {CardModule} from 'primeng/card';
+import {SkeletonModule} from 'primeng/skeleton';
 
 @Component({
 	standalone: true,
-	imports: [],
+	imports: [
+		DashboardHeaderComponent,
+		HeavyLoadersSlowComponent,
+		ProgressSpinnerModule,
+		CardModule,
+		SkeletonModule
+	],
 	templateUrl: './defer-views.component.html',
 	styles: ``
 })
--- src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CardModule} from 'primeng/card';
 
 @Component({
-	selector: 'app-heavy-loaders',
+	selector: 'heavy-loader-slow',
 	standalone: true,
-	imports: [],
+	imports: [
+		CardModule
+	],
 	templateUrl: './heavy-loaders-slow.component.html',
 	styles: ``
 })
 export class HeavyLoadersSlowComponent {
+
+	@Input({required: true}) cssClass!: string;
+
+	constructor() {
+		const start = Date.now();
+
+		while (Date.now() - start < 1500) {
+			// Código bloqueante
+		}
+	}
 
 }
