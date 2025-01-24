75c3c5906d34112040f0ec11ed9e25bf98fbe69d
Escoz <kyrovilches@gmail.com>
2025-01-24 11:42:32 +0100

    Detección de cambios en change-detection.component

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,15 +1,47 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
 import {DashboardHeaderComponent} from '../../../shared/dashboard-header/dashboard-header.component';
+import {CardModule} from 'primeng/card';
+import {JsonPipe} from '@angular/common';
+import {DividerModule} from 'primeng/divider';
+import {PanelModule} from 'primeng/panel';
 
 @Component({
 	selector: 'app-change-detection',
 	standalone: true,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [
-		DashboardHeaderComponent
+		DashboardHeaderComponent,
+		CardModule,
+		JsonPipe,
+		DividerModule,
+		PanelModule
 	],
 	templateUrl: './change-detection.component.html',
 	styles: ``
 })
 export default class ChangeDetectionComponent {
+
+	public currentFramework = computed(() =>
+		`Cambio de detección en Angular - ${this.frameworkAsSignal().name}`);
+
+	public frameworkAsSignal = signal({
+		name: 'Angular',
+		version: '17',
+		releaseDate: '2023'
+	});
+
+	public frameworkAsProperty = {
+		name: 'Angular',
+		version: '17',
+		releaseDate: '2023'
+	};
+
+	constructor() {
+		setTimeout(() => {
+			this.frameworkAsSignal.update(value =>
+				({...value, name: 'React'}));
+
+		}, 3000);
+	}
 
 }
